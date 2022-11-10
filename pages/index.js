4aff2447aa62cc626a64435b41457af9932749f1
Enzo Nakahara <japanakah@outlook.com>
2022-11-10 18:13:24 -0300

    Favorites add

--- pages/index.js
+++ pages/index.js
@@ -1,138 +1,129 @@
-import styled from 'styled-components'
-import { useState } from 'react'
+import styled from "styled-components";
+import { useState } from "react";
 
-import config from '../config.json'
-import Menu from '../src/components/Menu';
-import { CSSReset } from '../src/components/CSSReset';
-import { StyledTimeline } from '../src/components/TimeLine';
-import { StyledFavorites } from '../src/components/Favorites';
+import config from "../config.json";
+import Menu from "../src/components/Menu";
+import { CSSReset } from "../src/components/CSSReset";
+import { StyledTimeline } from "../src/components/TimeLine";
+import { StyledFavorites } from "../src/components/Favorites";
 
 function HomePage() {
-    const [filterValue, setFilterValue] = useState('');
+  const [filterValue, setFilterValue] = useState("");
 
-    return (
-        <>
-            <CSSReset />
-            <div style={{
-                display: "flex",
-                flexDirection: "column",
-                flex: 1,
-            }}>
-                <Menu filterValue={filterValue} setFilterValue={setFilterValue} />
-                <Header imageSrc={config.mainBanner} />
-                <TimeLine searchValue={filterValue} playlists={config.playlists} />
-                <Favorites favorites={config.favorites} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <CSSReset />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+        }}
+      >
+        <Menu filterValue={filterValue} setFilterValue={setFilterValue} />
+        <Header imageSrc={config.mainBanner} />
+        <TimeLine searchValue={filterValue} playlists={config.playlists} />
+        <Favorites favorites={config.favorites} />
+      </div>
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
 
 const StyledHeader = styled.div`
-    .avatar {
-        width: 80px;
-        height: 80px;
-        border-radius: 50%;
-        box-shadow: 0px 0px 5px black;
-    }
-    .user-info {
-        display: flex;
-        align-items: center;
-        width: 100%;
-        padding: 16px 32px;
-        gap: 16px;
-        box-shadow: 0px 5px 10px rgba(70, 130, 180, .5);
-    }
+  .avatar {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    box-shadow: 0px 0px 5px black;
+  }
+  .user-info {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+    box-shadow: 0px 5px 10px rgba(70, 130, 180, 0.5);
+  }
 `;
 const StyledBanner = styled.div`
-    background-image: url(${config.bg});
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    height: 250px;
-
-`
+  background-image: url(${config.bg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  height: 250px;
+`;
 
 function Header(props) {
-
-    return (
-        <StyledHeader>
-            <StyledBanner />
-            <section className="user-info">
-                <img className="avatar" src={`https://github.com/${config.github}.png`} />
-                <div>
-                    <h2>
-                        {config.name}
-                    </h2>
-                    <p>
-                        {config.job}
-                    </p>
-                </div>
-            </section>
-        </StyledHeader>
-    )
+  return (
+    <StyledHeader>
+      <StyledBanner />
+      <section className="user-info">
+        <img
+          className="avatar"
+          src={`https://github.com/${config.github}.png`}
+        />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.job}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  );
 }
 
 function TimeLine({ searchValue, ...props }) {
-    const playlistsNames = Object.keys(props.playlists);
+  const playlistsNames = Object.keys(props.playlists);
 
-    return (
-        <StyledTimeline>
-            {playlistsNames.map((playlistName) => {
-                const videos = props.playlists[playlistName];
-                return (
-                    <section key={playlistName}>
-                        <h2>{playlistName}</h2>
-                        <div>
-                            {videos
-                                .filter((video) => {
-                                    const titleNormalized = video.title.toLowerCase();
-                                    const searchValueNormalized = searchValue.toLowerCase();
+  return (
+    <StyledTimeline>
+      {playlistsNames.map((playlistName) => {
+        const videos = props.playlists[playlistName];
+        return (
+          <section key={playlistName}>
+            <h2>{playlistName}</h2>
+            <div>
+              {videos
+                .filter((video) => {
+                  const titleNormalized = video.title.toLowerCase();
+                  const searchValueNormalized = searchValue.toLowerCase();
 
-                                    return titleNormalized.includes(searchValueNormalized)
-                                })
-                                .map((video) => {
-                                    return (
-                                        <a key={video.url} href={video.url}>
-                                            <img src={video.thumb} />
-                                            <span>
-                                                {video.title}
-                                            </span>
-                                        </a>
-                                    )
-                                })}
-                        </div>
-                    </section>
-                )
-            })}
-        </StyledTimeline>
-    )
+                  return titleNormalized.includes(searchValueNormalized);
+                })
+                .map((video) => {
+                  return (
+                    <a key={video.url} href={video.url}>
+                      <img src={video.thumb} />
+                      <span>{video.title}</span>
+                    </a>
+                  );
+                })}
+            </div>
+          </section>
+        );
+      })}
+    </StyledTimeline>
+  );
 }
 
 function Favorites(props) {
+  const favorites = props.favorites;
+  console.log(favorites);
 
-    const favorites = props.favorites
-    console.log(favorites);
-
-    return (
-        <StyledFavorites>
-            {favorites
-                .map((favorites) => {
-                    return (
-                        <section key={favorites}>
-                            <h2>Favoritos</h2>
-                            <div>
-                                <a key={favorites.url} href={favorites.url}>
-                                    <img className="favorite-avatar" src={favorites.avatar} />
-                                    <span>
-                                        {favorites.name}
-                                    </span>
-                                </a>
-                            </div>
-                        </section>
-                    )
-                })}
-        </StyledFavorites>
-    )
+  return (
+    <StyledFavorites>
+      <h2> Favoritos </h2>
+      <section>
+        {favorites.map((favorites) => {
+          return (
+            <a key={favorites.channelUrl} href={favorites.channelUrl}>
+              <img className="favorite-avatar" src={favorites.channelAvatar} />
+              <span>{favorites.name}</span>
+            </a>
+          );
+        })}
+      </section>
+    </StyledFavorites>
+  );
 }
--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,12 +1,8 @@
 import styled from "styled-components";
 
 export const StyledFavorites = styled.div`
-  flex: 1;
-  width: 100%;
-  padding: 16px;
-  overflow: hidden;
-
   .favorite-avatar {
+    margin-top: 5px;
     width: 80px;
     height: 80px;
     border-radius: 50%;
@@ -14,40 +10,26 @@ export const StyledFavorites = styled.div`
   }
   h2 {
     font-size: 16px;
-    margin-bottom: 16px;
+    margin: 0px 0px 16px 32px;
     text-transform: capitalize;
   }
-  img {
-    aspect-ratio: 16/9;
-    font-weight: 500;
-    object-fit: cover;
-    width: 100%;
-    max-width: 210px;
-    height: auto;
-  }
   section {
-    width: 100%;
-    padding: 0;
-    overflow: hidden;
-    padding: 16px;
-    div {
-      
-      width: calc(100vw - 16px * 4);
-      display: grid;
-      grid-gap: 16px;
-      grid-template-columns: repeat(auto-fill,minmax(200px,1fr));
-      grid-auto-flow: column;
-      grid-auto-columns: minmax(200px,1fr);
-      overflow-x: scroll;
-      scroll-snap-type: x mandatory;
-      a {
-        scroll-snap-align: start;
-        span {
-          padding-top: 8px;
-          display: block;
-          padding-right: 24px;
-          color: ${({ theme }) => theme.textColorBase || "#222222"};
-        }
+    width: calc(100vw - 16px * 4);
+    display: flex;
+    flex-direction: row;
+    overflow-x: scroll;
+    scroll-snap-type: x mandatory;
+    margin-left: 32px;
+    a {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      scroll-snap-align: start;
+      margin: 0px 10px;
+      span {
+        padding-top: 8px;
+        display: block;
+        color: ${({ theme }) => theme.textColorBase || "#222222"};
       }
     }
   }
